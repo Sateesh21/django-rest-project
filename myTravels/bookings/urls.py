b3b07d4b11d6af57cf0a69cbbfcf13b3e5f4de05
sateesh21 <sateeshs402.it@gmail.com>
2025-06-25 15:19:35 +0530

    files are added

---
+++ myTravels/bookings/urls.py
@@ -0,0 +1,12 @@
+from .views import RegisterView, LoginView, BusListCreateView,BusDetailView, UserBookingView,BookingView
+from django.urls import path
+
+urlpatterns = [
+    path('buses/', BusListCreateView.as_view(), name='buslist'),
+    path('buses/<int:pk>/', BusDetailView.as_view(), name='bus-detail'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('user/<int:user_id>/bookings/', UserBookingView.as_view(), name='user-bookings'),
+    path('booking/', BookingView.as_view(), name='booking')
+
+]
